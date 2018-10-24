2436f7279f30d3caa1a07aa95b525c9ad60f170c
x_studying <xg_55@126.com>
2018-10-24 18:23:05 +0800

    UE项目

---
+++ Demo/ChineseChess/UE/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace UE
+{
+    struct FString
+    {
+
+    }
+
+    public class UObject
+    {
+        long handle;
+
+        [MethodImpl(MethodImplOptions.InternalCall)]
+        extern static FString GetName(long h);
+
+        FString GetName()
+        {
+            return GetName(handle);
+        }
+    }
+
+    public class UComponent:UObject
+    {
+
+    }
+}
