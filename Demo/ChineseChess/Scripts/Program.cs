f4cc2b5e5a3ad56017d4587bdcf043c978259949
x_studying <xg_55@126.com>
2018-10-25 19:28:27 +0800

    lua编译

--- Demo/ChineseChess/Scripts/Program.cs
+++ Demo/ChineseChess/Scripts/Program.cs
@@ -8,7 +8,6 @@ namespace ChineseChessCS
         {
             base.BeginPlay();
             Log("BeginPlay");
-
         }
     }
 }
--- Demo/ChineseChess/Scripts/build.bat
+++ Demo/ChineseChess/Scripts/build.bat
@@ -1,2 +1,3 @@
 ::"../UL/UL/Bin/CSharpCompiler.exe" pj_csharp_compiler.json
-"../UL/UL/Bin/CppConverter.exe" pj_cppconverter.json
+::"../UL/UL/Bin/CppConverter.exe" pj_cppconverter.json
+"../UL/UL/Bin/CppConverter.exe" pj_luaconverter.json
